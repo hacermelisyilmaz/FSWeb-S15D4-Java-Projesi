0b7b6d312136797386b2b29367eb06c579e1daac
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-30 16:03:12 +0300

    Letters are copied to the Stack from ArrayList

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,9 @@ public class Main {
         String plainText = text.replaceAll("[.,?!'_-]", "").replaceAll(" ", "").toLowerCase(Locale.ENGLISH);
 
         List<String> letters = new ArrayList<>(Arrays.asList(plainText.split("")));
+
         Stack<String> lettersStack = new Stack<>();
+        lettersStack.addAll(letters);
 
     }
 }
